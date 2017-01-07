82a145717976f16aa161a6f9cb0eb6e47bc66d33
TH.Laptop <THansenite@gmail.com>
2017-01-06 21:59:37 -0600

    Added 2009 Schedule files and scripted seasons table

--- README.md
+++ README.md
@@ -2,7 +2,21 @@
 
 ###Tasks###
 * [ ] Write scripts to create tables
-	* [ ] Seasons
+	* [x] Seasons
 	* [ ] Teams
+		* [ ] 2009-10 Teams
+		* [ ] 2010-11 Teams
+		* [ ] 2011-12 Teams
+		* [ ] 2012-13 Teams
+		* [ ] 2013-14 Teams
+		* [ ] 2014-15 Teams
+		* [ ] 2015-16 Teams
 	* [ ] Schedule
+		* [ ] 2009-10 Schedule
+		* [ ] 2010-11 Schedule
+		* [ ] 2011-12 Schedule
+		* [ ] 2012-13 Schedule
+		* [ ] 2013-14 Schedule
+		* [ ] 2014-15 Schedule
+		* [ ] 2015-16 Schedule
 * [ ] Bootstrap Angular site
--- Scripts/Seasons.sql
+++ Scripts/Seasons.sql
@@ -1,8 +1,46 @@
+CREATE TABLE `season` (
+  `id` int(9) NOT NULL,
+  `name` char(50) NOT NULL,
+  `type` char(11) NOT NULL,
+  UNIQUE KEY `id` (`id`)
+)
 
+INSERT into season (name, type) VALUES("2009 Preseason", "PRE");
+INSERT into season (name, type) VALUES("2009-10 Scramble", "SCR");
+INSERT into season (name, type) VALUES("2009-10 Regular Season", "REG");
+INSERT into season (name, type) VALUES("2010 Playoffs", "PLY");
 
-INSERT into 
+INSERT into season (name, type) VALUES("2010 Preseason", "PRE");
+INSERT into season (name, type) VALUES("2010-11 Scramble", "SCR");
+INSERT into season (name, type) VALUES("2010-11 Regular Season", "REG");
+INSERT into season (name, type) VALUES("2011 Playoffs", "PLY");
 
-("2009-10 Regular Season", "REG")
-("2010 Playoffs", "PLF")
-("2010-11 Regular Season", "REG")
-("2011 Playoffs", "PLF")
+INSERT into season (name, type) VALUES("2011 Preseason", "PRE");
+INSERT into season (name, type) VALUES("2011-12 Scramble", "SCR");
+INSERT into season (name, type) VALUES("2011-12 Regular Season", "REG");
+INSERT into season (name, type) VALUES("2012 Playoffs", "PLY");
+
+INSERT into season (name, type) VALUES("2012 Preseason", "PRE");
+INSERT into season (name, type) VALUES("2012-13 Scramble", "SCR");
+INSERT into season (name, type) VALUES("2012-13 Regular Season", "REG");
+INSERT into season (name, type) VALUES("2013 Playoffs", "PLY");
+
+INSERT into season (name, type) VALUES("2013 Preseason", "PRE");
+INSERT into season (name, type) VALUES("2013-14 Scramble", "SCR");
+INSERT into season (name, type) VALUES("2013-14 Regular Season", "REG");
+INSERT into season (name, type) VALUES("2014 Playoffs", "PLY");
+
+INSERT into season (name, type) VALUES("2014 Preseason", "PRE");
+INSERT into season (name, type) VALUES("2014-15 Scramble", "SCR");
+INSERT into season (name, type) VALUES("2014-15 Regular Season", "REG");
+INSERT into season (name, type) VALUES("2015 Playoffs", "PLY");
+
+INSERT into season (name, type) VALUES("2015 Preseason", "PRE");
+INSERT into season (name, type) VALUES("2015-16 Scramble", "SCR");
+INSERT into season (name, type) VALUES("2015-16 Regular Season", "REG");
+INSERT into season (name, type) VALUES("2016 Playoffs", "PLY");
+
+INSERT into season (name, type) VALUES("2016 Preseason", "PRE");
+INSERT into season (name, type) VALUES("2016-17 Scramble", "SCR");
+INSERT into season (name, type) VALUES("2016-17 Regular Season", "REG");
+INSERT into season (name, type) VALUES("2017 Playoffs", "PLY");
